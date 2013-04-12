78541f05552fbaf18c460b05b2494ecb225ef210
Max Ogden <max@maxogden.com>
2013-04-12 12:24:42 -0700

    0.1.0 - add all minecraft block types

--- index.js
+++ index.js
@@ -1,252 +1,866 @@
-
-var block, blockInfo, key, n, _i;
-
-blockInfo = {
-  '_-1': {
+module.exports = {
+  "_-1": {
     id: -10,
-    type: 'fill',
-    rgba: [0.0, 0.0, 0.0, 1.0],
-    t: [0, 0]
+    type: "fill"
   },
-  '_0': {
-    id: 0,
-    type: 'air',
-    rgba: [1.0, 1.0, 1.0, 1.0],
-    t: [0, 0]
+  "_1": {
+    "type": "stone",
+    "id": 1
   },
-  '_1': {
-    id: 1,
-    type: 'stone',
-    rgba: [0.4, 0.4, 0.4, 1.0],
-    t: [1, 0]
+  "_2": {
+    "type": "grass",
+    "id": 2
   },
-  '_2': {
-    id: 2,
-    type: 'grass',
-    rgba: [0.0, 0.6, 0.0, 1.0],
-    t: [8, 2]
+  "_3": {
+    "type": "dirt",
+    "id": 3
   },
-  '_2x': {
-    id: 2,
-    type: 'grassside',
-    rgba: [0.0, 0.6, 0.0, 1.0],
-    t: [3, 0]
+  "_4": {
+    "type": "cobblestone",
+    "id": 4
   },
-  '_3': {
-    id: 3,
-    type: 'dirt',
-    rgba: [0.2, 0.0, 0.0, 1.0],
-    t: [2, 0]
+  "_5": {
+    "type": "wooden_plank",
+    "id": 5
   },
-  '_4': {
-    id: 4,
-    type: 'cobble',
-    rgba: [84, 94, 88, 1.0],
-    t: [0, 1]
+  "_6": {
+    "type": "sapling",
+    "id": 6
   },
-  '_5': {
-    id: 5,
-    type: 'wood',
-    rgba: [0.38, 0.20, 0.20, 1.0],
-    t: [4, 0]
+  "_7": {
+    "type": "adminium",
+    "id": 7
   },
-  '_8': {
-    id: 8,
-    type: 'water',
-    rgba: [0.0, 0.0, 1.0, 1.0],
-    t: [15, 13]
+  "_8": {
+    "type": "water",
+    "id": 8
   },
-  '_9': {
-    id: 9,
-    type: 'water2',
-    rgba: [0.0, 0.0, 1.0, 1.0],
-    t: [15, 13]
+  "_9": {
+    "type": "stationary_water",
+    "id": 9
   },
-  '_12': {
-    id: 12,
-    type: 'sand',
-    rgba: [250, 250, 200, 1.0],
-    t: [2, 1]
+  "_10": {
+    "type": "lava",
+    "id": 10
   },
-  '_11': {
-    id: 11,
-    type: 'lava2',
-    rgba: [250, 135, 17, 1.0],
-    t: [15, 15]
+  "_11": {
+    "type": "stationary_lava",
+    "id": 11
   },
-  '_10': {
-    id: 10,
-    type: 'lava',
-    rgba: [250, 135, 20, 1.0],
-    t: [15, 15]
+  "_12": {
+    "type": "sand",
+    "id": 12
   },
-  '_13': {
-    id: 13,
-    type: 'gravel',
-    rgba: [186, 179, 186, 1.0],
-    t: [3, 1]
+  "_13": {
+    "type": "gravel",
+    "id": 13
   },
-  '_15': {
-    id: 15,
-    type: 'iron',
-    rgba: [226, 192, 170, 1.0],
-    t: [1, 2]
+  "_14": {
+    "type": "gold_ore",
+    "id": 14
   },
-  '_16': {
-    id: 16,
-    type: 'coal',
-    rgba: [0.0, 0.0, 0.0, 1.0],
-    t: [2, 2]
+  "_15": {
+    "type": "iron_ore",
+    "id": 15
   },
-  '_17': {
-    id: 17,
-    type: 'log',
-    rgba: [0.18, 0.0, 0.0, 1.0],
-    t: [4, 1]
+  "_16": {
+    "type": "coal_ore",
+    "id": 16
   },
-  '_18': {
-    id: 18,
-    type: 'leaves',
-    rgba: [0.0, 0.30, 0.0, 1.0],
-    t: [4, 3]
+  "_17": {
+    "type": "wood",
+    "id": 17
   },
-  '_20': {
-    id: 20,
-    type: 'glass',
-    rgba: [0.9, 0.9, 0.9, 0.3],
-    t: [1, 3]
+  "_18": {
+    "type": "leaves",
+    "id": 18
   },
-  '_24': {
-    id: 24,
-    type: 'sandstone',
-    rgba: [0.9, 0.9, 0.9, 0.3],
-    t: [0, 13]
+  "_19": {
+    "type": "sponge",
+    "id": 19
   },
-  '_35': {
-    id: 35,
-    type: 'wool',
-    rgba: [0.9, 0.9, 0.9, 0.3],
-    t: [0, 4]
+  "_20": {
+    "type": "glass",
+    "id": 20
   },
-  '_45': {
-    id: 45,
-    type: 'brick',
-    rgba: [0.5, 0.0, 0.0, 1.0],
-    t: [7, 0]
+  "_21": {
+    "type": "lapis_lazuli_ore",
+    "id": 21
   },
-  '_48': {
-    id: 48,
-    type: 'mossy',
-    rgba: [165, 187, 181, 1.0],
-    t: [4, 2]
+  "_22": {
+    "type": "lapis_lazuli_block",
+    "id": 22
   },
-  '_50': {
-    id: 50,
-    type: 'torch',
-    rgba: [0.92, 0.57, 0.13, 1.0],
-    t: [0, 5],
-    s: 1
+  "_23": {
+    "type": "dispenser",
+    "id": 23
   },
-  '_52': {
-    id: 52,
-    type: 'spawner',
-    rgba: [209, 240, 139, 1.0],
-    t: [1, 4]
+  "_24": {
+    "type": "sandstone",
+    "id": 24
   },
-  '_53': {
-    id: 53,
-    type: 'woodstairs',
-    rgba: [0.38, 0.20, 0.20, 1.0],
-    t: [4, 0]
+  "_25": {
+    "type": "note_block",
+    "id": 25
   },
-  '_56': {
-    id: 56,
-    type: 'diamond',
-    rgba: [43, 199, 172, 1.0],
-    t: [2, 3]
+  "_26": {
+    "type": "colored_wool",
+    "id": 26
   },
-  '_60': {
-    id: 60,
-    type: 'farmland',
-    rgba: [0, 0, 0, 1.0],
-    t: [6, 5]
+  "_27": {
+    "type": "powered_rail",
+    "id": 27
   },
-  '_64': {
-    id: 64,
-    type: 'woodendoor',
-    rgba: [0, 0, 0, 1.0],
-    t: [1, 6],
-    s: 1
+  "_28": {
+    "type": "detector_rail",
+    "id": 28
   },
-  '_64x': {
-    id: 64,
-    type: 'woodendoor',
-    rgba: [0, 0, 0, 1.0],
-    t: [1, 5],
-    s: 1
+  "_29": {
+    "type": "sticky_piston",
+    "id": 29
   },
-  '_67': {
-    id: 67,
-    type: 'cobblestairs',
-    rgba: [84, 94, 88, 240],
-    t: [0, 1]
+  "_30": {
+    "type": "cobweb",
+    "id": 30
   },
-  '_78': {
-    id: 78,
-    type: 'snow',
-    rgba: [0.98, 0.98, 0.98, 1.0],
-    t: [2, 4]
+  "_31": {
+    "type": "grass",
+    "id": 31
   },
-  '_79': {
-    id: 79,
-    type: 'ice',
-    rgba: [41, 169, 255, 1.0],
-    t: [3, 4]
+  "_32": {
+    "type": "dead_bush",
+    "id": 32
   },
-  '_80': {
-    id: 80,
-    type: 'snowb',
-    rgba: [0.99, 0.99, 0.99, 1.0],
-    t: [4, 4]
+  "_33": {
+    "type": "piston",
+    "id": 33
   },
-  '_85': {
-    id: 85,
-    type: 'fence',
-    rgba: [0.38, 0.20, 0.20, 1.0],
-    t: [4, 0]
+  "_34": {
+    "type": "black_wool",
+    "id": 34
   },
-  '_102': {
-    id: 102,
-    type: 'glasspane',
-    rgba: [0.0, 0.0, 0.0, 1.0],
-    t: [1, 3],
-    s: 1
+  "_35": {
+    "type": "wool",
+    "id": 35
+  },
+  "_36": {
+    "type": "wool",
+    "id": 36
+  },
+  "_37": {
+    "type": "yellow_flower",
+    "id": 37
+  },
+  "_38": {
+    "type": "red_flower",
+    "id": 38
+  },
+  "_39": {
+    "type": "brown_mushroom",
+    "id": 39
+  },
+  "_40": {
+    "type": "red_mushroom",
+    "id": 40
+  },
+  "_41": {
+    "type": "gold_block",
+    "id": 41
+  },
+  "_42": {
+    "type": "iron_block",
+    "id": 42
+  },
+  "_43": {
+    "type": "double_stone_slab",
+    "id": 43
+  },
+  "_44": {
+    "type": "stone_slab",
+    "id": 44
+  },
+  "_45": {
+    "type": "brick",
+    "id": 45
+  },
+  "_46": {
+    "type": "tnt",
+    "id": 46
+  },
+  "_47": {
+    "type": "bookshelf",
+    "id": 47
+  },
+  "_48": {
+    "type": "moss_stone",
+    "id": 48
+  },
+  "_49": {
+    "type": "obsidian",
+    "id": 49
+  },
+  "_50": {
+    "type": "torch",
+    "id": 50
+  },
+  "_51": {
+    "type": "fire",
+    "id": 51
+  },
+  "_52": {
+    "type": "monster_spawner",
+    "id": 52
+  },
+  "_53": {
+    "type": "wooden_stairs",
+    "id": 53
+  },
+  "_54": {
+    "type": "chest",
+    "id": 54
+  },
+  "_55": {
+    "type": "redstone_wire",
+    "id": 55
+  },
+  "_56": {
+    "type": "diamond_ore",
+    "id": 56
+  },
+  "_57": {
+    "type": "diamond_block",
+    "id": 57
+  },
+  "_58": {
+    "type": "workbench",
+    "id": 58
+  },
+  "_59": {
+    "type": "wheat_seeds",
+    "id": 59
+  },
+  "_60": {
+    "type": "soil",
+    "id": 60
+  },
+  "_61": {
+    "type": "furnace",
+    "id": 61
+  },
+  "_62": {
+    "type": "burning_furnace",
+    "id": 62
+  },
+  "_63": {
+    "type": "signpost",
+    "id": 63
+  },
+  "_64": {
+    "type": "wooden_door",
+    "id": 64
+  },
+  "_65": {
+    "type": "ladder",
+    "id": 65
+  },
+  "_66": {
+    "type": "minecart_track",
+    "id": 66
+  },
+  "_67": {
+    "type": "cobblestone_stairs",
+    "id": 67
+  },
+  "_68": {
+    "type": "wall_sign",
+    "id": 68
+  },
+  "_69": {
+    "type": "lever",
+    "id": 69
+  },
+  "_70": {
+    "type": "stone_pressure_plate",
+    "id": 70
+  },
+  "_71": {
+    "type": "iron_door",
+    "id": 71
+  },
+  "_72": {
+    "type": "wooden_pressure_plate",
+    "id": 72
+  },
+  "_73": {
+    "type": "redstone_ore",
+    "id": 73
+  },
+  "_74": {
+    "type": "glowing_redstone_ore",
+    "id": 74
+  },
+  "_75": {
+    "type": "redstone_torch_off",
+    "id": 75
+  },
+  "_76": {
+    "type": "redstone_torch_on",
+    "id": 76
+  },
+  "_77": {
+    "type": "stone_button",
+    "id": 77
+  },
+  "_78": {
+    "type": "snow",
+    "id": 78
+  },
+  "_79": {
+    "type": "ice",
+    "id": 79
+  },
+  "_80": {
+    "type": "snow_block",
+    "id": 80
+  },
+  "_81": {
+    "type": "cactus",
+    "id": 81
+  },
+  "_82": {
+    "type": "clay",
+    "id": 82
+  },
+  "_83": {
+    "type": "sugar_cane",
+    "id": 83
+  },
+  "_84": {
+    "type": "jukebox",
+    "id": 84
+  },
+  "_85": {
+    "type": "fence",
+    "id": 85
+  },
+  "_86": {
+    "type": "pumpkin",
+    "id": 86
+  },
+  "_87": {
+    "type": "netherrack",
+    "id": 87
+  },
+  "_88": {
+    "type": "soul_sand",
+    "id": 88
+  },
+  "_89": {
+    "type": "glowstone",
+    "id": 89
+  },
+  "_90": {
+    "type": "portal",
+    "id": 90
+  },
+  "_91": {
+    "type": "jack-o-lantern",
+    "id": 91
+  },
+  "_92": {
+    "type": "cake",
+    "id": 92
+  },
+  "_95": {
+    "type": "locked_chest",
+    "id": 95
+  },
+  "_96": {
+    "type": "trapdoor",
+    "id": 96
+  },
+  "_97": {
+    "type": "monster_egg",
+    "id": 97
+  },
+  "_98": {
+    "type": "stone_brick",
+    "id": 98
+  },
+  "_99": {
+    "type": "huge_brown_mushroom",
+    "id": 99
+  },
+  "_100": {
+    "type": "huge_red_mushroom",
+    "id": 100
+  },
+  "_101": {
+    "type": "iron_bars",
+    "id": 101
+  },
+  "_102": {
+    "type": "glass_pane",
+    "id": 102
+  },
+  "_103": {
+    "type": "melon",
+    "id": 103
+  },
+  "_106": {
+    "type": "vines",
+    "id": 106
+  },
+  "_107": {
+    "type": "fence_gate",
+    "id": 107
+  },
+  "_108": {
+    "type": "brick_stairs",
+    "id": 108
+  },
+  "_109": {
+    "type": "stone_brick_stairs",
+    "id": 109
+  },
+  "_110": {
+    "type": "mycelium",
+    "id": 110
+  },
+  "_111": {
+    "type": "lily_pad",
+    "id": 111
+  },
+  "_112": {
+    "type": "nether_brick",
+    "id": 112
+  },
+  "_113": {
+    "type": "nether_brick_fence",
+    "id": 113
+  },
+  "_114": {
+    "type": "nether_brick_stairs",
+    "id": 114
+  },
+  "_116": {
+    "type": "enchantment_table",
+    "id": 116
+  },
+  "_121": {
+    "type": "end_stone",
+    "id": 121
+  },
+  "_122": {
+    "type": "dragon_egg",
+    "id": 122
+  },
+  "_123": {
+    "type": "redstone_lamp",
+    "id": 123
+  },
+  "_126": {
+    "type": "wooden_slab",
+    "id": 126
+  },
+  "_127": {
+    "type": "cocoa_plant",
+    "id": 127
+  },
+  "_128": {
+    "type": "sandstone_stairs",
+    "id": 128
+  },
+  "_129": {
+    "type": "emerald_ore",
+    "id": 129
+  },
+  "_130": {
+    "type": "ender_chest",
+    "id": 130
+  },
+  "_133": {
+    "type": "block_of_emerald",
+    "id": 133
+  },
+  "_134": {
+    "type": "spruce_wood_stairs",
+    "id": 134
+  },
+  "_135": {
+    "type": "birch_wood_stairs",
+    "id": 135
+  },
+  "_136": {
+    "type": "jungle_wood_stairs",
+    "id": 136
+  },
+  "_137": {
+    "type": "command_block",
+    "id": 137
+  },
+  "_138": {
+    "type": "beacon",
+    "id": 138
+  },
+  "_139": {
+    "type": "cobblestone_wall",
+    "id": 139
+  },
+  "_143": {
+    "type": "wooden_button",
+    "id": 143
+  },
+  "_145": {
+    "type": "anvil",
+    "id": 145
+  },
+  "_260": {
+    "type": "apple",
+    "id": 260
+  },
+  "_262": {
+    "type": "arrow",
+    "id": 262
+  },
+  "_263": {
+    "type": "coal",
+    "id": 263
+  },
+  "_264": {
+    "type": "diamond",
+    "id": 264
+  },
+  "_265": {
+    "type": "iron_ingot",
+    "id": 265
+  },
+  "_266": {
+    "type": "gold_ingot",
+    "id": 266
+  },
+  "_280": {
+    "type": "stick",
+    "id": 280
+  },
+  "_281": {
+    "type": "bowl",
+    "id": 281
+  },
+  "_282": {
+    "type": "mushroom_soup",
+    "id": 282
+  },
+  "_287": {
+    "type": "string",
+    "id": 287
+  },
+  "_288": {
+    "type": "feather",
+    "id": 288
+  },
+  "_289": {
+    "type": "gun_powder",
+    "id": 289
+  },
+  "_295": {
+    "type": "seeds",
+    "id": 295
+  },
+  "_296": {
+    "type": "wheat",
+    "id": 296
+  },
+  "_297": {
+    "type": "bread",
+    "id": 297
+  },
+  "_318": {
+    "type": "flint",
+    "id": 318
+  },
+  "_319": {
+    "type": "raw_porkchop",
+    "id": 319
+  },
+  "_320": {
+    "type": "cooked_porkchop",
+    "id": 320
+  },
+  "_321": {
+    "type": "paintings",
+    "id": 321
+  },
+  "_322": {
+    "type": "golden_apple",
+    "id": 322
+  },
+  "_323": {
+    "type": "sign",
+    "id": 323
+  },
+  "_324": {
+    "type": "wooden_door",
+    "id": 324
+  },
+  "_325": {
+    "type": "bucket",
+    "id": 325
+  },
+  "_326": {
+    "type": "water_bucket",
+    "id": 326
+  },
+  "_327": {
+    "type": "lava_bucket",
+    "id": 327
+  },
+  "_329": {
+    "type": "saddle",
+    "id": 329
+  },
+  "_330": {
+    "type": "iron_door",
+    "id": 330
+  },
+  "_331": {
+    "type": "redstone_dust",
+    "id": 331
+  },
+  "_332": {
+    "type": "snowball",
+    "id": 332
+  },
+  "_333": {
+    "type": "boat",
+    "id": 333
+  },
+  "_334": {
+    "type": "leather",
+    "id": 334
+  },
+  "_335": {
+    "type": "milk",
+    "id": 335
+  },
+  "_336": {
+    "type": "clay_brick",
+    "id": 336
+  },
+  "_337": {
+    "type": "clay_balls",
+    "id": 337
+  },
+  "_338": {
+    "type": "sugar_cane",
+    "id": 338
+  },
+  "_339": {
+    "type": "paper",
+    "id": 339
+  },
+  "_340": {
+    "type": "book",
+    "id": 340
+  },
+  "_341": {
+    "type": "slimeball",
+    "id": 341
+  },
+  "_344": {
+    "type": "egg",
+    "id": 344
+  },
+  "_346": {
+    "type": "fishing_rod",
+    "id": 346
+  },
+  "_348": {
+    "type": "glowstone_dust",
+    "id": 348
+  },
+  "_349": {
+    "type": "raw_fish",
+    "id": 349
+  },
+  "_350": {
+    "type": "cooked_fish",
+    "id": 350
+  },
+  "_351": {
+    "type": "dyes",
+    "id": 351
+  },
+  "_352": {
+    "type": "bone",
+    "id": 352
+  },
+  "_353": {
+    "type": "sugar",
+    "id": 353
+  },
+  "_354": {
+    "type": "cake",
+    "id": 354
+  },
+  "_356": {
+    "type": "redstone_repeater",
+    "id": 356
+  },
+  "_357": {
+    "type": "cookie",
+    "id": 357
+  },
+  "_358": {
+    "type": "map",
+    "id": 358
+  },
+  "_359": {
+    "type": "shears",
+    "id": 359
+  },
+  "_360": {
+    "type": "melon_slice",
+    "id": 360
+  },
+  "_361": {
+    "type": "pumpkin_seeds",
+    "id": 361
+  },
+  "_362": {
+    "type": "melon_seeds",
+    "id": 362
+  },
+  "_363": {
+    "type": "raw_beef",
+    "id": 363
+  },
+  "_364": {
+    "type": "steak",
+    "id": 364
+  },
+  "_365": {
+    "type": "raw_chicken",
+    "id": 365
+  },
+  "_366": {
+    "type": "cooked_chicken",
+    "id": 366
+  },
+  "_367": {
+    "type": "rotton_flesh",
+    "id": 367
+  },
+  "_368": {
+    "type": "ender_pearl",
+    "id": 368
+  },
+  "_369": {
+    "type": "blaze_rod",
+    "id": 369
+  },
+  "_370": {
+    "type": "ghast_tear",
+    "id": 370
+  },
+  "_374": {
+    "type": "glass_bottle",
+    "id": 374
+  },
+  "_375": {
+    "type": "spider_eye",
+    "id": 375
+  },
+  "_376": {
+    "type": "fermented_spider_eye",
+    "id": 376
+  },
+  "_377": {
+    "type": "blaze_powder",
+    "id": 377
+  },
+  "_378": {
+    "type": "magma_cream",
+    "id": 378
+  },
+  "_379": {
+    "type": "brewing_stand",
+    "id": 379
+  },
+  "_380": {
+    "type": "cauldron",
+    "id": 380
+  },
+  "_381": {
+    "type": "eye_of_ender",
+    "id": 381
+  },
+  "_382": {
+    "type": "glistering_melon",
+    "id": 382
+  },
+  "_383": {
+    "type": "spawn_eggs",
+    "id": 383
+  },
+  "_384": {
+    "type": "bottle_o_enchanting",
+    "id": 384
+  },
+  "_385": {
+    "type": "fire_charge",
+    "id": 385
+  },
+  "_388": {
+    "type": "emerald",
+    "id": 388
+  },
+  "_390": {
+    "type": "flower_pot",
+    "id": 390
+  },
+  "_391": {
+    "type": "carrot",
+    "id": 391
+  },
+  "_392": {
+    "type": "unknown",
+    "id": 392
+  },
+  "_393": {
+    "type": "baked_potato",
+    "id": 393
+  },
+  "_394": {
+    "type": "poisonous_potato",
+    "id": 394
+  },
+  "_395": {
+    "type": "map",
+    "id": 395
+  },
+  "_396": {
+    "type": "golden_carrot",
+    "id": 396
+  },
+  "_397": {
+    "type": "mob_head",
+    "id": 397
+  },
+  "_399": {
+    "type": "nether_star",
+    "id": 399
+  },
+  "_400": {
+    "type": "pumkpin_pie",
+    "id": 400
+  },
+  "_401": {
+    "type": "firework_rocket",
+    "id": 401
+  },
+  "_402": {
+    "type": "firework_star",
+    "id": 402
   }
 };
-
-for (key in blockInfo) {
-  block = blockInfo[key];
-  if (block.rgba[0] > 1 || block.rgba[1] > 1 || block.rgba[2] > 1) {
-    for (n = _i = 0; _i <= 2; n = ++_i) {
-      block.rgba[n] = block.rgba[n] / 255.0;
-    }
-  }
-}
-
-/*
-to find texture coord:
-
-first of all, what is x and y position of 
-subtexture for that block?
-
-blocks = { 1: stone, rgb, [0,0]
-
-upper left = 
-x / 16,  y/16
-
-lower right = (x+1/16), (y+1/16)
-*/
-
-
-module.exports = blockInfo;
-
